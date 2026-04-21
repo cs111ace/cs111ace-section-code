85d12302717d5ba5f3c48b451c5bd33d6c0e5969
fabio <fabioi@stanford.edu>
2026-04-21 13:53:12 -0700

    chore: add library.cpp
    Signed-off-by: fabio <fabioi@stanford.edu>

---
+++ 3-scheduling-&-linking/library.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int task(int id) {
+    std::cout << "hello from " << id << std::endl;
+    return id;
+}
--- 3-scheduling-&-linking/main.cpp
+++ 3-scheduling-&-linking/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <unordered_map>
 #include <map>
+#include "library.cpp"
 
 int main()
 {
-
+	// hashamp that I initialized
 	std::unordered_map<int, std::string> map{{1, "Fabio"}};
+
+	// iterating through map
 	for (auto elem : map)
 	{
 		std::cout << elem.first << std::endl;
 	}
+
+	task(2);
 	return 0;
 }
